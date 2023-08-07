b84b8ec5923501893d7ef098222669eee0a2c307
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-07 16:20:53 +0200

    maj TodoBusinessImplMockTest.java

--- src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -27,5 +30,26 @@ public class TodoBusinessImplMockTest {
         List<String> filteredTodos = todoBusinessImpl
                  .retrieveTodosRelatedToSpring("Dummy");
          assertEquals(2, filteredTodos.size());
+    }
+
+    @Test
+    public void testRetrieveTodosRelatedToSpring_usingBDD() {
+
+        // Given
+        TodoService todoServiceMock = mock(TodoService.class);
+
+        List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to dance");
+
+        given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
+
+        TodoBusinessImpl todoBusinessImpl =
+                new TodoBusinessImpl(todoServiceMock);
+
+        // When
+        List<String> filteredTodos = todoBusinessImpl
+                .retrieveTodosRelatedToSpring("Dummy");
+
+        // Then
+        assertThat(filteredTodos.size(), is(2));
     }
 }
